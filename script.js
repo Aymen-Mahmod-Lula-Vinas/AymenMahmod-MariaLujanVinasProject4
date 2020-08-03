48d57156854026afc6e339ee0304a079bf2f8b1a
Maria <lu_vinas@hotmail.com>
2020-08-02 21:42:18 -0400

    some JS touches :bulb:

--- script.js
+++ script.js
@@ -1,92 +1,108 @@
-// listen to a click event- user choice- that will return a value. create a variable to store the result
-// use the result to filter the user choice through all the types of alcohol 
-//  display 3 cocktails containing the selected type of alcohol
-// filter will display the results as an array- we will need to then choose ONLY the info that we want to gather (name, ingredients, instructions, img)
+
 // print it on the HTML
 
-// DOES THE LOGIC USED ABOVE MAKE ANY SENSE?
-
-
-
-// $(document).ready(function(){
-
-//     const alcoholTypes =  $('section#userChoice div p').toArray();
-//     console.log(alcoholTypes);
-
-//     // $('.button').on('click', function(){
-//     //     const userSelection = $(this).text();
-//     //     console.log(userSelection);
-        
-//     // });
-//   });
-  
-    
+// namespace
 const cocktailApp = {};
+let variable = [];
+// listen to a click event- user choice- that will return a value. create a variable to store the result
+$(".button").on('click', function(event) {
+    event.preventDefault();
+    // console.log(this.id);
+    cocktailApp.userChoice = this.id;
+    // console.log(userChoice);
+    cocktailApp.getIngredient(cocktailApp.userChoice);
+});
 
-cocktailApp.key = '1';
-cocktailApp.url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${userSelection}';
-
-cocktailApp.getCategories = () => {
+// first API call: filter API by type of alcohol selected (user choice) 
+cocktailApp.getIngredient = (userInput) => {
     $.ajax({
-        url: cocktailApp.url,
+        url: `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${userInput}`,
         method: 'GET',
         dataType: 'json',
     }).then((res) => {
-        // const firstDrink = res[0].strDrink;
-        // console.log(res.drinks);
-        // console.log(firstDrink);
-        // console.log(res.drinks[0].strDrink);
-        // anything that goes here will have access to the data from the wine search api
-        cocktailApp.showResults(res.drinks);
+      //  display 3 cocktails containing the selected type of alcohol
+        for (let i = 0; i <= 3; i++) {
+            // console.log(res.drinks[i]);
+            cocktailApp.firstDrink = res.drinks[i].idDrink;
+            cocktailApp.secondApiCall(cocktailApp.firstDrink);
+        } 
     })
 }
-// why cant i do the below - aymen wants to ask this in help cue
-// const drink = cocktailApp.getCategories()
-// console.log(drink);
-cocktailApp.showResults = (drink) => {
-    // get the image url and other data
-    // smush photos
-    drink.forEach((drinkObj) => {
+
+// second API call: filter API by drink Id) 
+
+cocktailApp.secondApiCall = (drinkId) => {
+    $.ajax({
+        url: `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`,
+        method: 'GET',
+        dataType: 'json',
+    }).then((res) => {
+        console.log(res);
+        const drinkResults = res.drinks;
+        cocktailApp.showResults(drinkResults);
+    })
+}
+// iterate over anything that includes string ingredient
+// make an array filled with those attributes
+// for loop length, if null skip it, else add to the array
+cocktailApp.showResults = (drinkObj) => {
+    // console.log(drink);
+    // add a remove
         console.log(drinkObj);
         $('.showHere').append(`
         <div>
-            <h2>${drinkObj.strDrink}</h2>
+            <h2>${drinkObj[0].strDrink}</h2>
         </div>
         <h3>What you will need:</h3>
         <ul>
             <li>
-            ${drinkObj.strIngredient1}
+            ${drinkObj[0].strIngredient1}
             </li>
             <li>
-            ${drinkObj.strIngredient2}
+            ${drinkObj[0].strIngredient2}
             </li>
             <li>
-            ${drinkObj.strIngredient3}
+            ${drinkObj[0].strIngredient3}
             </li>
             <li>
-            ${drinkObj.strIngredient4}
+            ${drinkObj[0].strIngredient4}
             </li>
         </ul>
         <div>
             <h3>This is how you'll make it:</h2>
-            <p>${drinkObj.strInstructions}</p>
+            <p>${drinkObj[0].strInstructions}</p>
         </div>
         <div class="imageDrink">
-        <img src=${drinkObj.strDrinkThumb} />
+            <img src=${drinkObj[0].strDrinkThumb} />
         </div>
         <div>
-        <p>${drinkObj.strInstructions}</p>
-        </div>
-        <div>
-        <h3>Measurements?</h2>
-        <p>Tailor it to your liking!</p> 
+            <h3>Measurements?</h2>
+            <p>Tailor it to your liking!</p> 
         </div>
     `);
-    })
-// how do I list all the ingredients while skipping the nulls? do I use for in? for each?? length?  Right now I manually putting each ingreident but it also show nulls for drinks that dont have 4 ingrideints etc
 }
+
 $(function() {
-    cocktailApp.getCategories();
 })
+// init function to start everything
+cocktailApp.init = function(){
+  cocktailApp.getIngredient();
+  
+
+}
+// doc ready
+$(function(){
+  cocktailApp.init();
+  
+})
+
+
+
+
+
+
+
+
+
 
 
